035a97745558eab9e238afd82caf81dfe7859262
wojciech1871 <wojciech1871@gmail.com>
2019-05-11 20:52:14 +0200

    sqls

--- hive/create_meteo_final.sql
+++ hive/create_meteo_final.sql
@@ -1,31 +1,11 @@
 CREATE EXTERNAL TABLE IF NOT EXISTS meteo_final_text(
-  data_pom timestamp,
-  stacja string,
-  sr_temp_powietrza double,
-  maks_temp_powietrza double,
-  min_temp_powietrza double,
-  min_temp_powietrza_grunt double,
-  sr_wilgotnosc_powietrza double,
-  sr_preznosc_pary_wodnej double,
-  sr_zachmurzenie double,
-  sr_predkosc_wiatru double,
-  czas_trwania_wiatru_pow_10 double,
-  czas_trwania_wiatru_pow_15 double,
-  cisnienie_na_poziomie_morza double,
-  cisnienie_na_poziomie_stacji double,
-  dobowa_suma_opadów double,
-  opad_dzien double,
-  opad_noc double,
-  wysokosc_pokrywy_snieznej double,
-  rownowaznik_wodny_sniegu double,
-  uslonecznienie double,
-  czas_trwania_opadu_deszczu double,
-  czas_trwania_opadu_sniegu double,
-  czas_trwania_opadu_deszczu_ze_sniegiem double,
-  czas_trwania_mgly double,
-  czas_trwania_rosy double,
-  czas_trwania_szronu double,
-  czas_trwania_burzy double)
+    dt timestamp ,
+    station string,
+    main_temp_max double,
+    main_temp_min double,
+    main_pressure double,
+    main_humidity double,
+    wind_speed double)
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY ','
 STORED AS TEXTFILE
@@ -33,33 +13,13 @@ LOCATION '/data/weather/meteo_final'
 TBLPROPERTIES ('skip.header.line.count'='1');
 
 CREATE TABLE IF NOT EXISTS meteo_final(
-  data_pom timestamp,
-  stacja string,
-  sr_temp_powietrza double,
-  maks_temp_powietrza double,
-  min_temp_powietrza double,
-  min_temp_powietrza_grunt double,
-  sr_wilgotnosc_powietrza double,
-  sr_preznosc_pary_wodnej double,
-  sr_zachmurzenie double,
-  sr_predkosc_wiatru double,
-  czas_trwania_wiatru_pow_10 double,
-  czas_trwania_wiatru_pow_15 double,
-  cisnienie_na_poziomie_morza double,
-  cisnienie_na_poziomie_stacji double,
-  dobowa_suma_opadów double,
-  opad_dzien double,
-  opad_noc double,
-  wysokosc_pokrywy_snieznej double,
-  rownowaznik_wodny_sniegu double,
-  uslonecznienie double,
-  czas_trwania_opadu_deszczu double,
-  czas_trwania_opadu_sniegu double,
-  czas_trwania_opadu_deszczu_ze_sniegiem double,
-  czas_trwania_mgly double,
-  czas_trwania_rosy double,
-  czas_trwania_szronu double,
-  czas_trwania_burzy double)
-STORED AS ORC
+    dt timestamp ,
+    station string,
+    main_temp_max double,
+    main_temp_min double,
+    main_pressure double,
+    main_humidity double,
+    wind_speed double)
+STORED AS ORC;
 
 INSERT OVERWRITE TABLE meteo_final SELECT * FROM meteo_final_text;
