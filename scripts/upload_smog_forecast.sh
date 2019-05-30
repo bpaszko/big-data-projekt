1f3bced42ed2c05ef416e909ca3ab04f0cd97241
MichalTurski <turski.michal.antoni@gmail.com>
2019-05-30 19:10:16 +0200

    Fixed bug in upload_forecast: comas no longer separates 2-part names

--- scripts/upload_smog_forecast.sh
+++ scripts/upload_smog_forecast.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-hive -e 'select * from smog_prediction where dt > current_date;' \
-| sed 's/[[:space:]]\+/,/g' > /tmp/smog_pred.csv
+hive -e 'set hive.cli.print.header=true; select * from smog_prediction where dt > current_date;' \
+| sed 's/[\t]/,/g' > /tmp/smog_pred.csv
 gsutil cp /tmp/smog_pred.csv gs://smog_pred_bucket/
 rm /tmp/smog_pred.csv
 
